701e72059f9d2bac1ffb51dc1fe3c7172f7d0a0e
chima <chimaataman@gmail.com>
2020-06-12 14:20:35 +0100

    handle duplicate entries with prefix 'www.'

--- subdomain_lookup.py
+++ subdomain_lookup.py
@@ -11,11 +11,16 @@ def get_subdomain_subpr(domain):
     base_dir = os.path.dirname(__file__)
     subprocess.run(["sublist3r", "-d", domain, "-o", os.path.join(base_dir, "subdomain_list.json")])
     subdomains = open(os.path.join(base_dir, "subdomain_list.json"), 'r').readlines()
-    subdomains_new = []
+    subdomains_new = set()
     for line in subdomains:
         line = line.strip()
         if line:
-            subdomains_new.append(line)
+            tmp = line.split('.')
+            if tmp[0] == 'www' and '.'.join(tmp[1:]) != domain:
+                line = line.replace('www.', '')
+            subdomains_new.add(line)
+    subdomains_new = list(subdomains_new)
+    subdomains_new = sorted(subdomains_new)
     with open(os.path.join(base_dir, "subdomain_list.json"), 'w') as file:
         file.write(json.dumps(subdomains_new))
 
